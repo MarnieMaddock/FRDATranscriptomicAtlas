abbe8798d58e4a9e3cdad122637fd3d7e59603b7
Marnie Maddock <marnie.maddock@hotmail.com>
2026-02-02 12:25:21 +1100

    comment

--- app.R
+++ app.R
@@ -2,7 +2,7 @@ suppressPackageStartupMessages({
   library(shiny)
 })
 
-# Source your package-like R/ files
+# Source R/ files
 invisible(lapply(list.files("R", pattern = "\\.R$", full.names = TRUE), source, local = TRUE))
 
 # Run
