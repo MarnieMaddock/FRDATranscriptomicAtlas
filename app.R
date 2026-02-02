9a7975d273d5bd8a4a7ed7d3dc28024c372df761
Marnie Maddock <marnie.maddock@hotmail.com>
2026-02-02 12:24:47 +1100

    suppress Loading required package: shiny

--- app.R
+++ app.R
@@ -1,4 +1,6 @@
-library(shiny)
+suppressPackageStartupMessages({
+  library(shiny)
+})
 
 # Source your package-like R/ files
 invisible(lapply(list.files("R", pattern = "\\.R$", full.names = TRUE), source, local = TRUE))
